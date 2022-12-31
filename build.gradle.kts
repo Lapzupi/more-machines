7763c89dfc864df57441be82345f374b4d22cb82
Omer Oreg <sarhatabaot1@gmail.com>
2022-12-31 17:55:30 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.configurationcache.extensions.capitalized
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.lapzupi.dev"
-version = "0.0.5"
+version = "0.0.6"
 
 val mojangMapped = project.hasProperty("mojang-mapped") || System.getProperty("mojang-mapped") != null
 
