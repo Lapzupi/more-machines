86ab6b1e632b78437c24a1aad4d3a4c3feef673f
Omer Oreg <omeroreg@gmail.com>
2023-07-03 17:32:38 +0300

    Update item registration to latest nova version

--- src/main/kotlin/com/lapzupi/dev/registry/Items.kt
+++ src/main/kotlin/com/lapzupi/dev/registry/Items.kt
@@ -1,19 +1,19 @@
 package com.lapzupi.dev.registry
 
 import com.lapzupi.dev.MoreMachinesAddon
-import xyz.xenondevs.nova.material.NovaMaterialRegistry.registerDefaultItem
+import xyz.xenondevs.nova.addon.registry.ItemRegistry
+import xyz.xenondevs.nova.initialize.Init
 
 /**
  *
  * @author sarhatabaot
  */
-object Items {
+@Init
+object Items: ItemRegistry by MoreMachinesAddon.registry {
 
     //IRON PLATE + EMPTY MAP
-    val EMPTY_BLUEPRINT = registerDefaultItem(MoreMachinesAddon, "empty_blueprint")
-    val FILLED_BLUEPRINT = registerDefaultItem(MoreMachinesAddon, "filled_blueprint")
+    val EMPTY_BLUEPRINT = registerItem("empty_blueprint")
+    val FILLED_BLUEPRINT = registerItem("filled_blueprint")
 
     //Fertilizer types, we should be able to create different fertilizer types through different items combinations..
-
-    fun init() = Unit
 }
