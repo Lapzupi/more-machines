065a526434b95fcf5483592435e0692fcd417bb0
Omer Oreg <omeroreg@gmail.com>
2023-07-03 17:29:12 +0300

    Update blocks to latest nova

--- src/main/kotlin/com/lapzupi/dev/registry/Blocks.kt
+++ src/main/kotlin/com/lapzupi/dev/registry/Blocks.kt
@@ -6,23 +6,29 @@ import com.lapzupi.dev.tilentity.processing.BlueprintCrafter
 import com.lapzupi.dev.tilentity.processing.ElectricComposter
 import com.lapzupi.dev.tilentity.processing.Recycler
 import org.bukkit.Material
+import xyz.xenondevs.nova.addon.registry.BlockRegistry
 import xyz.xenondevs.nova.data.world.block.property.Directional
-import xyz.xenondevs.nova.item.tool.ToolCategory
-import xyz.xenondevs.nova.item.tool.ToolLevel
-import xyz.xenondevs.nova.material.BlockOptions
-import xyz.xenondevs.nova.material.NovaMaterialRegistry.registerTileEntity
+import xyz.xenondevs.nova.initialize.Init
+import xyz.xenondevs.nova.item.options.BlockOptions
+import xyz.xenondevs.nova.item.tool.VanillaToolCategories
+import xyz.xenondevs.nova.item.tool.VanillaToolTiers
 import xyz.xenondevs.nova.world.block.sound.SoundGroup
 
 
-object Blocks {
-    private val STONE = BlockOptions(3.0, listOf(ToolCategory.PICKAXE), ToolLevel.STONE, true, SoundGroup.STONE, Material.NETHERITE_BLOCK, true)
+@Init
+object Blocks: BlockRegistry by MoreMachinesAddon.registry {
+    private val STONE = BlockOptions(
+        3.0,
+        listOf(VanillaToolCategories.PICKAXE),
+        VanillaToolTiers.STONE,
+        true,
+        SoundGroup.STONE,
+        Material.NETHERITE_BLOCK,
+        true
+    )
 
-    val BLUEPRINT_CRAFTER = registerTileEntity(MoreMachinesAddon, "blueprint_crafter", STONE, ::BlueprintCrafter, properties = listOf(Directional.NORMAL))
-    val AUTO_CRAFTER = registerTileEntity(MoreMachinesAddon, "auto_crafter", STONE, ::AutoCrafter, properties = listOf(Directional.NORMAL))
-    val ELECTRIC_COMPOSTER = registerTileEntity(MoreMachinesAddon, "electric_composter", STONE, ::ElectricComposter, properties = listOf(Directional.NORMAL))
-    val RECYCLER = registerTileEntity(MoreMachinesAddon, "recycler", STONE, ::Recycler, properties = listOf(Directional.NORMAL))
-
-
-    fun init() = Unit
-
+    val BLUEPRINT_CRAFTER = tileEntity("blueprint_crafter", ::BlueprintCrafter).blockOptions(STONE).properties(Directional.NORMAL).register()
+    val AUTO_CRAFTER =  tileEntity("auto_crafter", ::AutoCrafter).blockOptions(STONE).properties(Directional.NORMAL).register()
+    val ELECTRIC_COMPOSTER = tileEntity("electric_composter", ::ElectricComposter).blockOptions(STONE).properties(Directional.NORMAL).register()
+    val RECYCLER = tileEntity("recycler", ::Recycler).blockOptions(STONE).properties(Directional.NORMAL).register()
 }
